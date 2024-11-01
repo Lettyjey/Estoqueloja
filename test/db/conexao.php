7516c51df0ba992a1fc02a3aa6288f5a2c6e95f9
Lettyjey <138585454+Lettyjey@users.noreply.github.com>
2024-11-01 11:47:12 -0300

    commit

---
+++ test/db/conexao.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username =  "root";
+$passaword = "";
+$dbname = "test";
+
+$conn = new mysqli($severname, $username, $passaword, $dbname);
+
+if ($conn->connect_error) {
+    die("Conção falhou:".$con->connect_error);
+}
+
+?>
