8e952c19da64583b6d829977bd0e73214b1b2ae7
Lettyjey <138585454+Lettyjey@users.noreply.github.com>
2024-11-06 12:04:15 -0300

    commit

--- estoqueloja/db/conexao.php
+++ estoqueloja/db/conexao.php
@@ -1,13 +1,12 @@
 <?php
 $servername = "localhost";
-$username =  "root";
-$passaword = "";
+$username = "root";
+$password = "";
 $dbname = "test";
 
-$conn = new mysqli($severname, $username, %passaword, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("Conção falhou:".$con->connect_error);
+    die("Conexão falhou: " . $conn->connect_error);
 }
-
 ?>
